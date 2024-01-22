e59f9f1e2c984a5381d304c0139c906907f1c6e0
Lauri <laurikamm@gmail.com>
2024-01-22 21:01:13 +0200

    add question connection to db

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -38,6 +38,15 @@ const Question = () => {
   });
 
   // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+    setIsSubmitting(true);
+
+    try {
+      await createQuestion({});
+    } catch (error) {
+      setIsSubmitting(false);
+    }
+  }
 
   const handleInputKeyDown = (
     e: React.KeyboardEvent<HTMLInputElement>,
