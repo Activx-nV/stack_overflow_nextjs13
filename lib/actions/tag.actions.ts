54940943d479a608f2da5e4657df515924aea6c6
Lauri <laurikamm@gmail.com>
2024-03-18 22:37:31 +0200

    create action to fetch all tags

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -2,7 +2,8 @@
 
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
-import { GetTopInteractedTagsParams } from './shared.types';
+import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.types';
+import Tag from '@/database/tag.model';
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -14,18 +15,26 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 
     if (!user) throw new Error('User not found');
 
-    // Find interactions for the user and group by tags
+    // Find interactions for the user and group by tags...
+    // Interaction...
 
     return [
-      {
-        _id: '1',
-        name: 'tag',
-      },
-      {
-        _id: '2',
-        name: 'tag2',
-      },
+      { _id: '1', name: 'tag' },
+      { _id: '2', name: 'tag2' },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
